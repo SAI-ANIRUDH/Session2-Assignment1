489c525fff2e6f7db8a3282e274d771b70a8b9b7
SAI-ANIRUDH <SAI-ANIRUDH@users.noreply.github.com>
2017-07-19 11:23:52 +0530

    Update Assignment2_1.java

--- src/Assignment2_1.java
+++ src/Assignment2_1.java
@@ -6,9 +6,9 @@ public class Assignment2_1 {
 
 	public static void main(String[] args) {
 
-int a;
-int b;
-int c;
+int a; // Declaring a //
+int b; //Declaring b //
+int c;  // Declaring c //
 Scanner sc = new Scanner(System.in); // Using Scanner Class to give inputs from user//
 System.out.println("Please input a");
 a= sc.nextInt();   // Giving input for a//
@@ -16,14 +16,16 @@ System.out.println("Please input b");
 b= sc.nextInt();   // Giving input for b//
 System.out.println("Please input c");
 c= sc.nextInt(); // Giving input for c//
-if ((a > b) && (a>c)) // Comparison between a , b and c// 
+if ((a > b) && (a>c)) // Comparison between a , b and c using Logical And Operators -- If this condition becomes true a will be maximum// 
 {
 	System.out.println("a is Maximum");
 }
 else  {
 	System.out.println("a is Minimum");
 	}
-if ((a < b) && (b >c)) {
+if ((a < b) && (b >c) // Comparison between a , b and c using Logical And Operators -- If this condition becomes true b will be maximum// 
+
+{
 	System.out.println("b is Maximum");
 }
 	
@@ -31,7 +33,8 @@ if ((a < b) && (b >c)) {
 		System.out.println("b is Minimum");
 		
 	}
-if ((c>a) && (c>b)){
+if ((c>a) && (c>b)) // Comparison between a , b and c using Logical And Operators -- If this condition becomes true c will be maximum// 
+    {
 	System.out.println("c is Maximum");
 }
 else {
